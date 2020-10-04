30c52af39e7588d0bbcf8a4ca2bf135626a8e72a
Artur-Petriv <artur.petriv@gmail.com>
2020-10-04 09:33:50 +0300

    Add slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
+import Swiper from "swiper/bundle"
 const $ = require(`jquery`)
 window.jQuery = $
 require(`jquery-mask-plugin`)
 
+
 document.addEventListener(`DOMContentLoaded`, function () {
     $(`#tel`).mask(`+38 (000) 000-00-00`)
+
+    const swiper = new Swiper(`.swiper-container`, {
+        grabCursor: false,
+        simulateTouch: false,
+        pagination: {
+            el: `.swiper-pagination`,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: `.swiper-button-next`,
+            prevEl: `.swiper-button-prev`,
+        },
+    })
 })
